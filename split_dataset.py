ba26cf4c7284ec42dba76709c49d7b0b06e8fc6c
harshaneo17 <harsha.god86@gmail.com>
2021-03-10 10:16:24 +0000

    Add files via upload
    split folders and subfolders to training and validation

---
+++ split_dataset.py
@@ -0,0 +1,5 @@
+
+# pip install split_folders
+import splitfolders
+splitfolders.ratio('C:\\Users\\ASUS\\Downloads\\pokemon', output='C:\\Users\\ASUS\\Downloads\\pokedex', seed=42, ratio=(0.9, 0, 0.1))  #enter input and output folder path, the ratio is training,validation and testing
+### including sub folders all split is performed
